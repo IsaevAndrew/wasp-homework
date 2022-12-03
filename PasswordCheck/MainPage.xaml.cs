78337e28942ecb257c6e02f9c3d2c7bf564b6320
IsaevAndrew <74855624+IsaevAndrew@users.noreply.github.com>
2022-12-03 21:14:45 +0300

    Add files via upload

---
+++ PasswordCheck/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace FirstProgramm;
+
+public partial class MainPage : ContentPage
+{
+    public string true_password = "1234";
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+    public void OnButtonClicked(object sender, EventArgs args)
+    {
+        Result.Text += (sender as Button).Text;
+    }
+    public void Check(object sender, EventArgs args)
+    {
+        if (Result.Text!=true_password)
+            Result.Text = "";
+        else
+        {
+            Result.Text = "Успешно!";
+            foreach (View a in ((sender as Button).Parent as Grid).Children.Cast<View>())
+                a.IsEnabled = false;
+        }
+    }
+    public void Deleat(object sender, EventArgs args)
+    {
+        Result.Text = "";
+    }
+}
+
